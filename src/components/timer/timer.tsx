91f76c484e553b062f74c05f6c99ef999cbea677
Johannes Becker <jbecker@student.tugraz.at>
2023-08-07 18:12:53 +0200

    time service

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,6 +1,7 @@
 import { component$, $, useSignal, useTask$, useVisibleTask$ } from "@builder.io/qwik"
 import styles from "./timer.module.css"
 import type { ActivitiyStore } from "~/routes"
+import { formatTime } from "~/services/time-formatter"
 
 interface TimerProps {
     store: ActivitiyStore
@@ -24,14 +25,6 @@ export default component$((props: TimerProps) => {
 
         localStorage.setItem("activities", JSON.stringify(props.store.activities));
         localStorage.setItem("lastEnd", props.store.lastEnd.toString());
-    });
-
-    const formatTime = $((duration: number) => {
-        const hours = Math.floor(duration / 3600);
-        const minutes = Math.floor((duration % 3600) / 60);
-        const seconds = Math.floor(duration % 60);
-
-        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     });
 
     useTask$(({ track, cleanup }) => {
---
+++ src/services/time-formatter.ts
@@ -0,0 +1,9 @@
+import { $ } from "@builder.io/qwik"
+
+export const formatTime = $((timestamp: number) => {
+    const hours = Math.floor(timestamp / 3600);
+    const minutes = Math.floor((timestamp % 3600) / 60);
+    const seconds = Math.floor(timestamp % 60);
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+});
