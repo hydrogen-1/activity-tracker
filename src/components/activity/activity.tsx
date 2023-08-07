e2e89d6b9f9f732d0782974d5c61d1a6b9dc2348
Johannes Becker <jbecker@student.tugraz.at>
2023-08-07 18:13:02 +0200

    better format

--- src/components/activity/activity.tsx
+++ src/components/activity/activity.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import styles from "./activity.module.css"
 import type { ActivitiyStore, Activity } from "~/routes";
+import { formatTime } from "~/services/time-formatter"
 
 interface ActivityProps {
     activity: Activity;
@@ -13,8 +14,11 @@ export default component$((props: ActivityProps) => {
     return (
         <div class={styles.card}>
             <p class={styles.dates}>
-                {startDate.toLocaleDateString()} {startDate.toLocaleTimeString()} -
-                {endDate.toLocaleDateString()} {endDate.toLocaleTimeString()}
+                {startDate.toLocaleTimeString()} -
+                {endDate.toLocaleTimeString()}
+            </p>
+            <p class={styles.duration}>
+                {formatTime(Math.floor((endDate.getTime() - startDate.getTime())/1000))}
             </p>
             <p class={styles.category}>{props.activity.category}</p>
             <p class={styles.name}>{props.activity.name}</p>
