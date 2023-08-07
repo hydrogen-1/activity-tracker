a168e8e0ef6306fe20466d43019174062dae9334
Johannes Becker <jbecker@student.tugraz.at>
2023-08-07 12:14:04 +0200

    add timer assembly

---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,12 @@
+import { component$ } from "@builder.io/qwik"
+
+export default component$(() => {
+    return (
+        <>
+            <div class="container">
+                <p>Timer</p>
+                <button>🛑</button>
+            </div>
+        </>
+    ) 
+})
