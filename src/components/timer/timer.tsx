80985e12809405319e0c194c1059cb5daba4fb85
Johannes Becker <jbecker@student.tugraz.at>
2023-08-09 12:42:51 +0200

    add reset button

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,58 +1,88 @@
-import { component$, $, useSignal, useTask$, useVisibleTask$, useContext } from "@builder.io/qwik"
-import styles from "./timer.module.css"
-import { ActivityContext } from "~/routes/layout"
-import { formatTime } from "~/services/time-formatter"
-import { v4 } from "uuid"
+import {
+  component$,
+  $,
+  useSignal,
+  useTask$,
+  useVisibleTask$,
+  useContext,
+} from "@builder.io/qwik";
+import styles from "./timer.module.css";
+import { ActivityContext } from "~/routes/layout";
+import { formatTime } from "~/services/time-formatter";
+import { v4 } from "uuid";
 
 export default component$(() => {
-    const store = useContext(ActivityContext);
-    const activity = useSignal("");
-    const category = useSignal("none");
-    const elapsedTime = useSignal(Date.now() - store.lastEnd);
+  const ctx = useContext(ActivityContext);
+  const activity = useSignal("");
+  const category = useSignal("none");
+  const elapsedTime = useSignal(Date.now() - ctx.lastEnd);
 
-    const stopCurrent = $(() => {
-        const currentTime = Date.now();
-        store.activities.push({
-            id: v4(),
-            start: store.lastEnd,
-            end: currentTime,
-            name: activity.value,
-            category: category.value
-        })
-        store.lastEnd = currentTime;
-        elapsedTime.value = 0;
-
-        localStorage.setItem("activities", JSON.stringify(store.activities));
-        localStorage.setItem("lastEnd", store.lastEnd.toString());
+  const stopCurrent = $(() => {
+    const currentTime = Date.now();
+    ctx.activities.push({
+      id: v4(),
+      start: ctx.lastEnd,
+      end: currentTime,
+      name: activity.value,
+      category: category.value,
     });
+    ctx.lastEnd = currentTime;
+    elapsedTime.value = 0;
 
-    useTask$(({ track, cleanup }) => {
-        track(() => elapsedTime.value);
-        const timer = setTimeout(() => (
-            elapsedTime.value++
-        ), 1000);
-        cleanup(() => clearTimeout(timer));
-    });
+    localStorage.setItem("activities", JSON.stringify(ctx.activities));
+    localStorage.setItem("lastEnd", ctx.lastEnd.toString());
+  });
 
-    useVisibleTask$(() => {
-        elapsedTime.value = (Date.now() - store.lastEnd) / 1000
-    })
+  useTask$(({ track, cleanup }) => {
+    track(() => elapsedTime.value);
+    const timer = setTimeout(() => elapsedTime.value++, 1000);
+    cleanup(() => clearTimeout(timer));
+  });
 
-    return (
-        <div class={styles.container}>
-            <p>{formatTime(elapsedTime.value)}</p>
-            <button onClick$={stopCurrent}>🛑</button>
-            <div class={styles.inputs}>
-                <label for="activity">Activity: </label>
-                <input type="text" id="activity" value={activity.value} onInput$={(el) => activity.value = (el.target as HTMLInputElement).value.trim()}/>
-                <label for="category">Category: </label>
-                <select name="category" id="category" value={category.value} onChange$={(el) => category.value = el.target.value}>
-                    <option value="none">none</option>
-                    {store.categories.map((category) => {
-                        return <option value={category} key={category}>{category}</option>
-                    })}
-                </select>
-            </div>
-        </div>
-    );
-})
+  useVisibleTask$(() => {
+    elapsedTime.value = (Date.now() - ctx.lastEnd) / 1000;
+  });
+
+  const reset = $(() => {
+    ctx.lastEnd = Date.now();
+    elapsedTime.value = 0;
+    localStorage.setItem("lastEnd", ctx.lastEnd.toString());
+  });
+
+  return (
+    <div class={styles.container}>
+      <p>{formatTime(elapsedTime.value)}</p>
+      <div class={styles.buttons}>
+        <button onClick$={reset}>🔄</button>
+        <button onClick$={stopCurrent}>🛑</button>
+      </div>
+      <div class={styles.inputs}>
+        <label for="activity">Activity: </label>
+        <input
+          type="text"
+          id="activity"
+          value={activity.value}
+          onInput$={(el) =>
+            (activity.value = (el.target as HTMLInputElement).value.trim())
+          }
+        />
+        <label for="category">Category: </label>
+        <select
+          name="category"
+          id="category"
+          value={category.value}
+          onChange$={(el) => (category.value = el.target.value)}
+        >
+          <option value="none">none</option>
+          {ctx.categories.map((category) => {
+            return (
+              <option value={category} key={category}>
+                {category}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </div>
+  );
+});
